1dc39359bf217dc57d37e92ac600472b83a2657a
Oleksandr Makohon <sashamakogon94@gmail.com>
2021-08-31 15:57:40 +0300

    Updates

---
+++ precache-manifest.dffcea09824ff04238f54d3d4592be1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02b13d5579f05dbdbff0227659590b59",
+    "url": "./index.html"
+  },
+  {
+    "revision": "79b8891a25cb4b36f450",
+    "url": "./static/css/main.12145b5b.chunk.css"
+  },
+  {
+    "revision": "a3fa78d2a47eeb043805",
+    "url": "./static/js/2.ac9e10eb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.ac9e10eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79b8891a25cb4b36f450",
+    "url": "./static/js/main.a006f7b4.chunk.js"
+  },
+  {
+    "revision": "8b3c3b96386eee1ea33a",
+    "url": "./static/js/runtime-main.b672bc02.js"
+  }
+]);
